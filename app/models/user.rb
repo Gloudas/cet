6a6c0d3fff8fd26ed8280ef87dcccf0ba931a5e4
Douglas Wreden <douglas@douglas-G71G.(none)>
2012-10-22 00:23:37 -0700

    added rspec tests for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,14 @@ class User < ActiveRecord::Base
 
   def self.find_or_create_from_auth_hash(auth_hash)
     # print out the auth_hash 
-    auth_hash.keys.each do |k|
-      print "key:    " + k + "\n"
-      print "value:    "
-      print auth_hash[k]
-      print "\n\n"
-    end
+#    auth_hash.keys.each do |k|
+#      print "key:    " + k + "\n"
+#      print "value:    "
+#      print auth_hash[k]
+#      print "\n\n"
+#    end
     # create a new user or retreive the user if he already exists
-    @user = User.find_by_uid(auth_hash["uid"])
+    @user = User.find_by_uid(auth_hash[:uid])
     if @user.nil?
       @user = User.create(:uid => auth_hash[:uid])
     end
