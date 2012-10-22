6559b932c74c1a8e79836b255e86cea8171e90d0
Douglas Wreden <douglas@douglas-G71G.(none)>
2012-10-21 23:19:18 -0700

    users are now created when you login with a new id

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Cet::Application.routes.draw do
   match '/auth/:provider/callback' => 'sessions#create'
   match '/:school' => 'schools#index', :as => 'school'
   match '/auth/failure', :to => 'sessions#failure'
+  match '/:school/login' => 'sessions#login', :as => 'login'
   match '/:school/logout' => 'sessions#destroy', :as => 'logout'
 
   # Sample resource route with options:
