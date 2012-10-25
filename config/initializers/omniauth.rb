90439bc7826eb20a682280eff41b32f9f83fbbbe
Douglas Wreden <douglas@douglas-G71G.(none)>
2012-10-25 14:33:28 -0700

    added omniauth-cas gem and included it in bundler

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer
   # example for 3rd party:
+  provider :cas, :host => 'cas.yourdomain.com'
+  # look here for more info: https://github.com/dlindahl/omniauth-cas 
   # provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+
 end
 
