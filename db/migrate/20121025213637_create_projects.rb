6bd0763b504aba19313975967fdd6b8b156c7af8
saasbook <saasbook@saasbook.(none)>
2012-10-28 15:43:22 -0700

    fixing migrations/models

--- db/migrate/20121025213637_create_projects.rb
+++ db/migrate/20121025213637_create_projects.rb
@@ -3,8 +3,6 @@ class CreateProjects < ActiveRecord::Migration
     create_table :projects do |t|
       t.string :title
       t.text :description
-      t.integer :creator_id
-      #t.integer :school_id
       t.timestamps
     end
   end
--- school.rb
+++
@@ -1,5 +0,0 @@
-
-class School < ActiveRecord::Base
-
-end
-
