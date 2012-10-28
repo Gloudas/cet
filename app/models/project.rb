e7060601954ba719bf506e8b17e6384c06a4d2ae
saasbook <saasbook@saasbook.(none)>
2012-10-28 15:34:11 -0700

    changing models, migrations, pt I

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,12 +1,13 @@
 class Project < ActiveRecord::Base
 
-  attr_accessible :title
+  attr_accessible :title, :description
 
   has_and_belongs_to_many :users
-  has_one :creator_id, :class_name => "User", :foreign_key => "user_id"
+  has_one :creator, :class_name => "User", :foreign_key => "id"
   belongs_to :school
   
-  validates_presence_of :creator_id
+  validates_presence_of :title
+  validates_presence_of :description
 
 
 end
