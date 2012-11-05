c628d0e6658eef73754fd98fd0257ca3f8a3c67a
Noel Moldvai <noelmoldvai@gmail.com>
2012-11-04 22:15:05 -0800

    profile controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,19 @@ class UsersController < ApplicationController
 
   def edit
     # user is set in the filter
-    if params[:project]
-      # process the form
-      params[:project]
+    if params[:profile]
+      # the form has been submitted
+      # update all the fields
+      params[:profile].each do |att, value|
+        @user[att] = value
+      end
+
+    success = @user.save
+    if success
+      redirect_to profile_path(@user.id) and return
+    else
+      flash[:error] = "Sorry, something went wrong with editing your profile"
+      redirect_to edit_profile_path
     end
   end
 
