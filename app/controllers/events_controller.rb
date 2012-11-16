6128c7bf981893b9d6fe647f05ac6441afd1875c
Steven Farberov <farberovs@yahoo.com>
2012-11-16 14:38:30 -0800

    date stuff works for events, but it will have to be changed to dateTime

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,14 +3,17 @@ class EventsController < ApplicationController
   def new
     @user = User.find(session[:user_id])
     event = params[:event]
-
-    Event.create(:name => event[:name], :description => event[:description], :time => event[:time], :date => event[:date])
+    date =  Date.new(event['date(1i)'].to_i, event['date(2i)'].to_i, event['date(3i)'].to_i)
+    #time = Time.new(event['time(3i)'].to_i, event['time(4i)'].to_i)
+    
+    Event.create(:name => event[:name], :description => event[:description], :date => date)
     redirect_to :action => 'index'
   end
 
   def index
     @user = User.find(session[:user_id])
     @events = Event.all
+    @event = Event.new
   end
 
   def edit
