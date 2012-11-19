c5896de08574c24ea7a97db00121bddc427bc5a0
Simon Janpanah <sjanpanah@Simons-MacBook-Pro.local>
2012-11-18 23:39:01 -0800

    initial document system setup - migrations, model, etc pt 2

---
+++ db/migrate/20121119072703_create_documents.rb
@@ -0,0 +1,10 @@
+class CreateDocuments < ActiveRecord::Migration
+  def change
+    create_table :documents do |t|
+      #t.text :description
+      t.integer :project_id
+      t.integer :uploader_id
+      t.timestamps
+    end
+  end
+end
