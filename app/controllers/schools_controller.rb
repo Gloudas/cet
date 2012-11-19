60051d8c9152f98aeb27f2567c8c92908eedb29a
Noel Moldvai <noelmoldvai@gmail.com>
2012-11-19 01:00:17 -0800

    some more chill updates

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -3,7 +3,10 @@ class SchoolsController < ApplicationController
   before_filter :set_current_user, :set_school
 
   def set_school
-    @school = School.find_by_uri(params[:school].downcase)
+    if params[:school] != params[:school].downcase
+      redirect_to school_path(params[:school].downcase) and return
+    end
+    @school = School.find_by_uri(params[:school])
     redirect_to home_path and return if not @school
   end
 
