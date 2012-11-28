92a17de5249531cd4ec90303b7c4c9004e313158
Steven Farberov <farberovs@yahoo.com>
2012-11-27 17:39:24 -0800

    added end time/location to events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,9 +3,10 @@ class EventsController < ApplicationController
   def new
     @user = User.find(session[:user_id])
     event = params[:event]
-    datetime = DateTime.new(event['time(1i)'].to_i, event['time(2i)'].to_i, event['time(3i)'].to_i, event['time(4i)'].to_i, event['time(5i)'].to_i)
-
-    Event.create!(:name => event[:name], :description => event[:description], :time => datetime, :school_id => @user.school)
+    startTime = DateTime.new(event['startTime(1i)'].to_i, event['startTime(2i)'].to_i, event['startTime(3i)'].to_i, event['startTime(4i)'].to_i, event['startTime(5i)'].to_i)
+    endTime = DateTime.new(event['endTime(1i)'].to_i, event['endTime(2i)'].to_i, event['endTime(3i)'].to_i, event['endTime(4i)'].to_i, event['endTime(5i)'].to_i)
+    
+    Event.create!(:name => event[:name], :description => event[:description], :location => event[:location], :startTime => startTime, :endTime => endTime, :school_id => @user.school)
     redirect_to :action => 'index'
   end
 
@@ -26,8 +27,11 @@ class EventsController < ApplicationController
     event = Event.find(params[:event_id])
     event.name  = event_info[:name]
     event.description = event_info[:description]
-    datetime = DateTime.new(event_info['time(1i)'].to_i, event_info['time(2i)'].to_i, event_info['time(3i)'].to_i, event_info['time(4i)'].to_i, event_info['time(5i)'].to_i)
-    event.time = datetime
+    event.location = event_info[:location]
+    startTime = DateTime.new(event_info['startTime(1i)'].to_i, event_info['startTime(2i)'].to_i, event_info['startTime(3i)'].to_i, event_info['startTime(4i)'].to_i, event_info['startTime(5i)'].to_i)
+    endTime = DateTime.new(event_info['endTime(1i)'].to_i, event_info['endTime(2i)'].to_i, event_info['endTime(3i)'].to_i, event_info['endTime(4i)'].to_i, event_info['endTime(5i)'].to_i)
+    event.startTime = startTime
+    event.endTime = endTime
     event.save
     redirect_to :action => 'index'
   end
--- db/migrate/20121113000321_create_events.rb
+++ db/migrate/20121113000321_create_events.rb
@@ -3,7 +3,9 @@ class CreateEvents < ActiveRecord::Migration
     create_table :events do |t|
       t.string :name
       t.text :description
-      t.datetime :time
+      t.datetime :startTime
+      t.datetime :endTime
+      t.string :location
       t.references :school
       t.timestamps
     end
