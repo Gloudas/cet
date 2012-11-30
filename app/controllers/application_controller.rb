f7e17d44bd299837e985f201683d5575c242c0a7
Noel Moldvai <noelmoldvai@gmail.com>
2012-11-30 03:19:14 -0800

    good home functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   def set_current_user
     # find_by_id(nil) returns nil
     @user ||= User.find_by_id(session[:user_id])
-    #redirect_to home_path and return unless @user
     if not @user
       flash[:error] = "Permission Denied...."
       redirect_to home_path and return
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
 
   def show
+    # if you're logged in, do not show this page
+    user = User.find_by_id(session[:user_id])
+    if user
+      redirect_to school_path(user.school.uri) and return
+    end
   end
 
 end
