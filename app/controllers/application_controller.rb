62341256f8bb49c0b519c2520fdadfc543b23ccf
Simon Janpanah <sjanpanah@Simons-MacBook-Pro.local>
2012-11-29 18:47:52 -0800

    flashing permission denied when redirecting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,11 @@ class ApplicationController < ActionController::Base
   def set_current_user
     # find_by_id(nil) returns nil
     @user ||= User.find_by_id(session[:user_id])
-    redirect_to home_path and return unless @user 
+    #redirect_to home_path and return unless @user
+    if not @user:
+      flash[:error] = "Permission Denied...."
+      redirect_to home_path and return
+    end
   end
   
   def time_ago(timestamp)
