08794d5002a44b1d1cda7ee5f0641b1cb3637ba9
Simon Janpanah <sjanpanah@Simons-MacBook-Pro.local>
2012-12-04 18:59:21 -0800

    adding confirmation messages for document and comment deletion, editing comment permissions to allow for anyone to comment but only delete their own messages if not a collaborator

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,31 +4,26 @@ class CommentsController < ApplicationController
   before_filter :set_can_edit
 
   def create
-    # can create only if you are collaborator
-    if @can_edit
-      # should just set the text field
-      comment = Comment.new(params[:comment])
-      comment.user = @user
-      comment.project = @project
-      if comment.save
-        flash[:notice] = "Commented!"
-      else
-        flash[:error] = "Oops, something went wrong with creating your comment."
-      end
+    # should just set the text field
+    comment = Comment.new(params[:comment])
+    comment.user = @user
+    comment.project = @project
+    if comment.save
+      flash[:notice] = "Commented!"
     else
-      flash[:error] = "You can't comment on this project!"
+      flash[:error] = "Oops, something went wrong with creating your comment."
     end
     redirect_to project_path(@project.id)
   end
 
   def destroy
     comment = Comment.find_by_id(params[:comid])
-    if @can_edit
-      # can destroy only if you are collaborator
+    if @can_edit or @user.id == comment.user.id
+      # can destroy only if you are collaborator or you created the comment
       if comment.destroy
         flash[:notice] = "Comment deleted!"
       else
-        flash[:error] = "Oops, something went wrong with deleting your comment."
+        flash[:error] = "Something went wrong with deleting your comment"
       end
     else
       flash[:error] = "You can't delete this comment!"
